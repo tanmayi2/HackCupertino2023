b7f90b7718605c2806b2721764f3415fddd1042b
Tanmayi Dasari <tanmayi.dasari@gmail.com>
2023-04-16 07:24:09 -0700

    small fixes

--- DashApp/app.py
+++ DashApp/app.py
@@ -9,19 +9,13 @@ from dash.dependencies import Input, Output, State
 
 app = Dash(__name__)
 
-# assume you have a "long-form" data frame
-# see https://plotly.com/python/px-arguments/ for more options
+#read csv dataset into pandas dataframe
 df = pd.read_csv("cuisine-32-hopefullyFinal3 - Sheet1.csv")
-# df = pd.read_excel("all_cuisines.xlsx", sheet_name='Sheet1', usecols=['title', 'nativeCuisine'])
-# df = pd.DataFrame({
-#     "Fruit": ["Apples", "Oranges", "Bananas", "Apples", "Oranges", "Bananas"],
-#     "Amount": [4, 1, 2, 2, 4, 5],
-#     "City": ["SF", "SF", "SF", "Montreal", "Montreal", "Montreal"]
-# })
 
+#map figure
 fig = px.choropleth(df, locations="country",
-                    color="nativeCuisine", # lifeExp is a column of gapminder
-                    hover_name="nativeCuisine", # column to add to hover information
+                    color="nativeCuisine", 
+                    hover_name="nativeCuisine", 
                     color_continuous_scale=px.colors.sequential.Plasma)
 fig.layout.plot_bgcolor = '#fff0d1'
 fig.layout.paper_bgcolor = '#fff0d1'
@@ -29,9 +23,9 @@ fig.update_layout(
     margin=dict(l=20,r=20,t=20,b=20),
     dragmode=False,
 )
-
 fig.show()
 
+#layout of the dashboard
 app.layout = html.Div(children=[
     html.Div(id='rectangle1'),
 
@@ -69,23 +63,17 @@ app.layout = html.Div(children=[
     html.Div(id='rectangle2'),
 ])
 
-
+#callback for interactions with the map
 @app.callback(
     Output('output', 'children'),
     Input('map', 'clickData'))
 def display_click_data(clickData):
-    #countryISO = clickData["points"]
     if not clickData:
         return "Nothing selected"
     countryISO = json.dumps(clickData["points"][0]['location'])
-    #return json.dumps(countryISO, indent=2)
     for ind in df.index:
         if df["country"][ind] == countryISO[1:-1] :
-            #print("hello")
             return json.dumps(df["title"][ind] + ": " + df["instructions"][ind], indent=2)
-            #print(df["instructions"][ind])
-
-    #return json.dumps(clickData, indent=2)
 
 if __name__ == '__main__':
     app.run_server(debug=True)
--- README.md
+++ README.md
@@ -15,3 +15,6 @@ Then, we used plotly and the dataset to create a map which catorogizes foods by 
 ![Hovering over a country displays the cuisine.](/ScreenPic.png)
 
 Clicking on the country generates a list of all the recipies listed from that cuisine.
+
+## **What's next for InFuse:**
+In the future, we hope to add more countries and cuisines to our map. Additionally, we want to provide more recipes when you click on each country and incorporate photos of the delicious dishes.
